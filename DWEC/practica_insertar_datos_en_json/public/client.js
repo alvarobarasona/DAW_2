646da4aaaf215aa520f7a2897a291403494fa6ac
alvarobarasona <dawjdc2022@gmail.com>
2024-02-15 12:55:17 +0100

    eliminando carpeta duplicada

--- DWEC/practica_insertar_datos_en_json/public/client.js
+++
@@ -1,8 +0,0 @@
-const addButton = document.getElementById("add-button");
-
-let inputValue;
-
-addButton.addEventListener("click", () => {
-    inputValue = document.getElementById("word-input").value;
-    console.log(inputValue);
-});
