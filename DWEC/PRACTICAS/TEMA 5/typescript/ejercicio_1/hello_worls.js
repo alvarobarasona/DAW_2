f44b0dd0b75f18769f83bdefd5adf84c644ba785
alvarobarasona <dawjdc2022@gmail.com>
2024-02-20 13:47:13 +0100

    subiendo ejercicio de introduccion a typescript

---
+++ DWEC/PRACTICAS/TEMA 5/typescript/ejercicio_1/hello_worls.js
@@ -0,0 +1,5 @@
+function greet(name) {
+    return "\u00A1Hello ".concat(name, "!");
+}
+var message = greet("World");
+console.log(message);
---
+++ DWEC/PRACTICAS/TEMA 5/typescript/ejercicio_1/hello_worls.ts
@@ -0,0 +1,6 @@
+function greet(name: string): string {
+    return `¡Hello ${name}!`;
+}
+
+const message = greet("World");
+console.log(message);
